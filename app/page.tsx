cbc6b60b8ee46a2ad3dae26ce542756adabe6966
Eddy Nathansyah <nathsyahed@gmail.com>
2023-10-26 17:38:51 +0700

    adding sidebar and snap scroll

--- app/page.tsx
+++ app/page.tsx
@@ -3,21 +3,21 @@ import { About, Experience, Homepage, Projects, Skills } from "@/components"
 export default function Home() {
 
   return (
-    <div className='w-full h-full'>
+    <div className='w-full h-screen overflow-scroll no-scrollbar snap-y snap-mandatory'>
       <Homepage
-        style='bg-gradient-to-b from-[#e7e4dc] from-90% to-[#e3ebee] to-100% text-black'
+        style='snap-start bg-gradient-to-b from-[#e7e4dc] from-90% to-[#e3ebee] to-100% text-black'
       />
       <About
-        style='bg-gradient-to-b from-[#e3ebee] from-85% to-[#f0ece2] to-100% text-black'
+        style='snap-start bg-gradient-to-b from-[#e3ebee] from-85% to-[#f0ece2] to-100% text-black'
       />
       <Experience
-        style='bg-[#f0ece2] text-black'
+        style='snap-start bg-[#f0ece2] text-black'
       />
       <Projects
-        style='bg-gradient-to-t from-[#e3ebee] from-85% to-[#f0ece2] to-100% text-black'
+        style='snap-start bg-gradient-to-t from-[#e3ebee] from-85% to-[#f0ece2] to-100% text-black'
       />
       <Skills
-        style='bg-gradient-to-t from-[#e7e4dc] from-85% to-[#e3ebee] to-96% text-black'
+        style='snap-start bg-gradient-to-t from-[#e7e4dc] from-85% to-[#e3ebee] to-96% text-black'
       />
     </div>
   )
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,30 +1,27 @@
-'use client'
-import { usePathname } from 'next/navigation'
-import React, { useMemo } from 'react'
+import React from 'react'
+import { BsArrowBarLeft } from 'react-icons/bs'
 
 interface SidebarProps {
     children: React.ReactNode
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ children }) => {
-    const pathName = usePathname()
-
-    const routes = useMemo(() => {
-        {
-            label: 'Home'
-            href: '/about'
-        }
-    }, [pathName])
 
     return (
-        <div className='bg-red-500 h-full flex'>
-            <div className='hidden lg:flex flex-col h-fit py-4 px-5 gap-y-2'>
-                {/* {routes.map((item) => (
-                    
-                ))} */}
-                tes
+        <div className='h-full flex'>
+            <div className='hidden lg:flex fixed hover:left-0 hover:transition-all transition-all left-[-100px] h-full w-max'>
+                <div className='h-full w-[100px] bg-slate-900 flex flex-col items-center gap-y-5 py-5'>
+                    <p>Home</p>
+                    <p>About</p>
+                    <p>Experience</p>
+                    <p>Project</p>
+                    <p>Language</p>
+                </div>
+                <div className='h-full w-[40px] flex items-center justify-center text-black scale-x-[-1]'>
+                    <BsArrowBarLeft size={28} />
+                </div>
             </div>
-            <main className='h-full'>
+            <main className='h-screen overflow-hidden'>
                 {children}
             </main>
         </div>
